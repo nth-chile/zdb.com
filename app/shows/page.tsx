abcf5c1a32ff23eb5c844de9e8bcbad46dce865d
Jared <jaredsalzano@gmail.com>
2023-12-06 14:19:59 -0500

    Adds comment for importing local version of fs-shows

--- app/shows/page.tsx
+++ app/shows/page.tsx
@@ -1,10 +1,11 @@
 import ClientPlayer from "../../src/ClientPlayer"
 import getData from 'fs-shows/dist/fs-shows-gcloud'
+// import getData from '../../../fs-shows/dist/fs-shows-gcloud'
 import 'fs-shows/dist/style.css'
 
 export default async function Shows() {
   const data = await getData('zdb-shows')
-  
+
   return <ClientPlayer
     title="Live Shows"
     data={data}
