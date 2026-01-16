5633f945a634e755a03bd0f3941a98b8f2798441
Jared Salzano <jaredsalzano@gmail.com>
2026-01-16 12:01:59 -0300

    feat: Use module imports for `fs-shows` and enable local development via Webpack alias.

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 zannaduband.com and zannadu.band
 
 IIRC Netlify deploys on push to main branch!
+
+Use `npm run dev:local-fs-shows` to run with local fs-shows lib
--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    ...(process.env.USE_LOCAL_FS_SHOWS_LIB === 'true' && {
+        webpack: (config) => {
+            config.resolve.alias['fs-shows'] = path.join(__dirname, '../fs-shows');
+            return config;
+        },
+    }),
+}
 
 module.exports = nextConfig
